defe21a77c1469fea7aa77f0d005342db6f5513c
aerhiellexx <2153674@slu.edu.ph>
2018-05-09 00:44:20 +0800

    Add files via upload

--- webfin.sql
+++ webfin.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
--- Host: localhost    Database: webfin
+-- Host: localhost    Database: webweb
 -- ------------------------------------------------------
 -- Server version	8.0.11
 
@@ -16,6 +16,53 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `migrations`
+--
+
+DROP TABLE IF EXISTS `migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `migrations`
+--
+
+LOCK TABLES `migrations` WRITE;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `password_resets`
+--
+
+DROP TABLE IF EXISTS `password_resets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `password_resets` (
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `password_resets`
+--
+
+LOCK TABLES `password_resets` WRITE;
+/*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user_account`
 --
 
@@ -23,16 +70,15 @@ DROP TABLE IF EXISTS `user_account`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_account` (
-  `first_name` varchar(45) NOT NULL,
-  `last_name` varchar(45) NOT NULL,
-  `middle_name` varchar(45) NOT NULL,
-  `gender` enum('F','M') NOT NULL,
-  `email_add` varchar(45) NOT NULL,
-  `birth_date` date NOT NULL,
-  `created_atu` varchar(45) DEFAULT NULL,
-  `updated_atu` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`email_add`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `first_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `middle_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `gender` enum('F','M') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email_add` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `birth_date` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_atu` timestamp NULL DEFAULT NULL,
+  `updated_atu` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +87,6 @@ CREATE TABLE `user_account` (
 
 LOCK TABLES `user_account` WRITE;
 /*!40000 ALTER TABLE `user_account` DISABLE KEYS */;
-INSERT INTO `user_account` VALUES ('Chris','Rodriguez','Carlos','M','chrisrodriguez@yahoo.com','1998-06-12',NULL,NULL),('John','Snow','Chris','M','johnsnow@yahoo.com','1997-09-28',NULL,NULL),('Sammie','Gonzalez','Garcia','F','sammiegonzalez@yahoo.com','1995-09-25',NULL,NULL),('Sarah','Williams','Lee','F','sarahwilliams@yahoo.com','1995-04-25',NULL,NULL),('Steven','Jones','David','M','stevenjones@yahoo.com','1996-02-14',NULL,NULL);
 /*!40000 ALTER TABLE `user_account` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -53,15 +98,18 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `remember_token` varchar(45) NOT NULL,
-  `created_at` varchar(45) NOT NULL,
-  `updated_at` varchar(45) NOT NULL,
-  PRIMARY KEY (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `lastname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `firstname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `middlename` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,6 +118,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,NULL,NULL,NULL,NULL,'crimelight24@gmail.com','$2y$10$zCxEpRTbZeAshKijotMGEOecOswgXwqe1KEN4lKw632xYlL7pfLIm','8SVAUSo0HKLKgQsVp2vktWq7t6JO16llsFQKs5jjdgjYU5JtCcywrORF58On','2018-05-02 13:04:19','2018-05-02 13:04:19'),(2,NULL,NULL,NULL,NULL,'qrst@gmail.com','$2y$10$7hmA2cw7zZ0zDzyVkOnoPe1L9f2aO4pFcSxycHzANmNOBSkK.ek5a','miKVJizaJPAoEexLc4VUECe6P6qjpTcvCIwjAjFX1MtfJE5lOpK2AhWlPzVU','2018-05-02 13:08:53','2018-05-02 13:08:53'),(3,NULL,NULL,NULL,NULL,'cdssf@gmail.com','$2y$10$dl6p9CZFoqlj95eY6PqIluM/PJNxaMyf2rUH7TITlROmz6pj9GFDO','BObScJcqpCkM0ZKh7JwcHPBRDMObcH1MbsTr9bHjJGJ2AD461DrgiIyykRt4','2018-05-02 13:22:30','2018-05-02 13:22:30'),(4,NULL,NULL,NULL,NULL,'asd@gmail.com','$2y$10$ScIQzhIEVLlkP6t9HEH6z.i/A.xwLXU2PXMq7YLa0H2WS4uK2M7.2','ZFGfQe3x9hTqqta5r4RP89Q8KKyZSPSMTS4azkOXy0B9TUOzjiHAn4QhAsnJ','2018-05-02 13:36:56','2018-05-02 13:36:56');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -82,4 +131,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-03  2:25:38
+-- Dump completed on 2018-05-09  0:11:39
