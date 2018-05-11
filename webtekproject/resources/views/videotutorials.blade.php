29d6e40c14ce2246d4034e01777e183215827b2a
aerhiellexx <2153674@slu.edu.ph>
2018-05-12 00:50:33 +0800

    Add video tutorials

---
+++ webtekproject/resources/views/videotutorials.blade.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<head>
+	<title>Video Tutorial</title>
+
+	<style>
+			.wrapper {
+				margin: 0 auto;
+				width: 80%;
+			}
+
+            .video1 {
+                position: relative;
+                padding-bottom: 56.25%;
+                padding-top: 25px;
+                height: 0px;
+            }
+
+            .video1 iframe {
+                position: absolute;
+                left: 0px;
+                top: 0px;
+                right: 0px;
+                bottom: 0px;
+                height: 100%;
+                width: 100%;
+            }
+
+            .video2 {
+                position: relative;
+                padding-bottom: 56.25%;
+                padding-top: 25px;
+                height: 0px;
+            }
+
+            .video2 iframe {
+                position: absolute;
+                left: 0px;
+                top: 0px;
+                right: 0px;
+                bottom: 0px;
+                height: 100%;
+                width: 100%;
+            }
+
+            #title {
+            	text-align: center;
+				font-weight: 400;
+				letter-spacing: 5;
+				font-family: rockwell;
+				line-height: 80px;
+            }
+    </style>
+</head>
+<body>
+	<div class="wrapper">
+		<h1 id = "title"> Servlet and JSP </h1>
+			<div class="video1">
+				<iframe width="420" height="315"
+				src="https://www.youtube.com/embed/tkFRGdUgCsE">
+				</iframe>
+			</div>
+		<h1 id = "title"> PHP </h1>
+			<div class="video2">
+				<iframe width="420" height="315"
+				src="https://www.youtube.com/embed/ZdP0KM49IVk">
+				</iframe>
+			</div>
+	</div>
+</body>
+</html>
